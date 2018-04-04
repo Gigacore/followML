967843d6c36b8352e0a7042f380324b77b5d115f
Gigacore <santhoshinbang@gmail.com>
2018-04-04 21:34:25 +0530

    logo + fixes

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -26,6 +26,7 @@ class SplashScreen extends Component {
     return (
       <div className={`splash-screen ${splash}`}>
         <div className="splash-content">
+          <img src="https://s3.amazonaws.com/follow-ml/brain.png" />
           <h2>Machine Learning needs human help!</h2>
           <p>At <strong>Multimodal Perception Lab, IIITB</strong> we are striving to automate the interview process.<br /> An integral part of this is to gather <span>follow-up questions</span> to make the process more natural.<br /> We need your help to accumulate follow-up questions for the already answered behavioral questions in mock interviews.</p>
           <p>Assume you are the interviewer or a hiring manager and fill in two follow-up questions for each of the question-answer pairs.<br /> Your contributions are appreciated and will be further used for research purposes. <strong>Thanks!</strong></p>
@@ -35,9 +36,9 @@ class SplashScreen extends Component {
           <div className="publications">
           <h3><strong>Our Publications:</strong></h3>
           <ol>
-            <li><span>Automatic Assessment of Communication Skill in Non-conventional Interview Settings: A Comparative Study. ICMI 2017</span></li>
-            <li><span>Online Peer-to-peer Discussions: A Platform for Automatic Assessment of Communication Skill. ACII Workshop 2017</span></li>
-            <li><span>Asynchronous Video Interviews vs. Face-to-Face Interviews for Communication Skill Measurement: A Systematic Study.</span></li>
+            <li><span><a href="https://dl.acm.org/citation.cfm?id=3136755.3136756" target="_blank">Automatic Assessment of Communication Skill in Non-conventional Interview Settings: A Comparative Study. ICMI 2017</a></span></li>
+            <li><span><a href="http://ieeexplore.ieee.org/abstract/document/8272588/" target="_blank">Online Peer-to-peer Discussions: A Platform for Automatic Assessment of Communication Skill. ACII Workshop 2017</a></span></li>
+            <li><span><a href="https://dl.acm.org/citation.cfm?id=2993183" target="_blank">Asynchronous Video Interviews vs. Face-to-Face Interviews for Communication Skill Measurement: A Systematic Study.</a></span></li>
           </ol>
           </div>
         </div>
